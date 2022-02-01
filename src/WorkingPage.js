6460bc0e140bd9c515df070e314938a588374a25
Tiffany Scott <tiffanyscott18@hotmail.com>
2022-02-01 01:51:22 -0500

    boilerplate-create

---
+++ src/WorkingPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const WorkingPage = () => (
+  <div>
+    <h1 className="test-style">OI FELIX COME HERE BRO</h1>
+  </div>
+);
+
+export default WorkingPage;
--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+import WorkingPage from "./WorkingPage";
 
-ReactDOM.render(<p>Boiler plate</p>, document.getElementById("app"));
+ReactDOM.render(<WorkingPage />, document.getElementById("app"));
